02e582b1966a52394cfe4bfac4752fd16ebee099
AmavelAntonio <amavelfranciscotomas@gmail.com>
2022-10-19 09:22:30 +0100

    aplicando os primeiros principios de clean-architecture

--- login.js
+++ login.js
@@ -7,6 +7,12 @@ const router = express.Router()
 class signUpRouter {
   async Route (req, res) {
     const { email, password, confirmPassword } = req.body
+    new SignUpUseCase().SignUp(email, password, confirmPassword)
+  }
+}
+
+class SignUpUseCase {
+  async SignUp (email, password, confirmPassword) {
     if (password === confirmPassword) {
       const user = await AccountModel.create({ email, password })
       return user
